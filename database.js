4c7ee1706a8d04220e39e14d07005f33a24c892d
khowtung <tttt39087@gmail.com>
2026-08-05 11:53:19 +0700

    Remove .env from Git tracking

--- database.js
+++ database.js
@@ -1,12 +1,13 @@
 const mysql = require('mysql2/promise');
 const fs = require("fs");
+require('dotenv').config();
 
 const database = mysql.createPool({
-    host: 'gateway01.ap-southeast-1.prod.aws.tidbcloud.com',
-    port: 4000,
-    user: '3yLVkRkeEWif26A.root',
-    password: 'vJU1pykkucYAYnJQ',
-    database: 'smart_village',
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
 
     waitForConnections: true,
     connectionLimit: 10,
